1303feedcb783e7caac12e1aa7e57b10b5dc56b3
Enes Kaan Bilir <bilireneskaan@gmail.com>
2023-04-13 12:24:50 +0300

    feat:tours adlı proje tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import './App.css';
 import Tours from './Tours';
+import './tours.css'
+import toursList from './data';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
+  const [data, setData] = useState(toursList)
 
+  function deleteList(id) {
+   const newFilter = data.filter( (e)=>e.id !==id )
+   setData(newFilter) 
+  }
+
+  const [load, setLoad] = useState(true);
+
+  const refreshPage = () => {
+    setLoad(true);
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 2000);
+  }, []);
+
+  
+  if (data.length === 0) {
+    return(
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }} >
+        <h2>No Tours Left</h2>
+        <button className='btn' onClick={refreshPage}>Refresh</button>
+      </div>
+    )
+  }
 
   return (
-    <div>
+    <div >
       <h2>Our Tours</h2>
       <div className='underLine' ></div>
-
-      <Tours />
+        <div className='tours'>
+     {
+      load ? <p className="p">Loading</p> : data.map( (e)=> <Tours data={e} del={deleteList} setLoad={setLoad} />  )
+     }
+     </div>
     </div>
   );
 }
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import './tours.css'
-function Tours() {
+function Tours({data,del,set}) {
    
+  const [showMore, setShowMore] = useState(false);
+
+  const shortText = data.info.split(" ").slice(0, 20).join(" ");
+
+
    return(
-   <div className='tours' >
-      <div className="single-tour">
-         <img src="" alt="" />
+   <article  >
+     <div className="single-tour">
+      <img src={data.image} alt="" />
          <div className="tour-info">
-            <span className="tour-price">19.90</span>
-            <h4>başlık</h4>
-            <p>bilgi
-            <button className='readMore'>Read More</button>
-            </p>
-            <button className="delete-btn">not interested</button>
-         </div>
+            <span className="tour-price">$ {data.price}</span>
+            <h4>{data.name}</h4>
+            <p>{showMore ? data.info : shortText}</p>
+      {showMore ? (
+        <button onClick={() => setShowMore(false)}>Show Less</button>
+      ) : (
+        <button onClick={() => setShowMore(true)}>Read More</button>
+      )}
+            <button className="delete-btn" onClick={()=>del(data.id)} >not interested</button>
       </div>
    </div>
+   </article>
+
    )
 }
 export default Tours;
